4d52276685312351b61f55653a0752cfbf60aed5
申丝露 <906592448@qq.com>
2017-07-26 15:34:38 +0800

    Initial Commit

---
+++ Share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Share
+//
+//  Created by 申丝露 on 2017/7/26.
+//  Copyright © 2017年 申丝露. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Share/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Share
+//
+//  Created by 申丝露 on 2017/7/26.
+//  Copyright © 2017年 申丝露. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
